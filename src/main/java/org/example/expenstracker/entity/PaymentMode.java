a5efd72c6165dff633ee089f3c3bdd80bca5528e
Vicky <vickykhambhatwala@gmail.com>
2026-09-19 15:18:26 +0530

    Expense Tracker

---
+++ src/main/java/org/example/expenstracker/entity/PaymentMode.java
@@ -0,0 +1,9 @@
+package org.example.expenstracker.entity;
+
+public enum PaymentMod {
+
+    CASH,
+    UPI,
+    QR_CODE
+
+}
---
+++ src/test/java/org/example/expenstracker/ExpensTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.expenstracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpensTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
